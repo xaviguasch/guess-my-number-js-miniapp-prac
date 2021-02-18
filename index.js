53caadc0ef735159468aaffb52307c61163a624d
xaviguasch <xaviguasch@gmail.com>
2021-02-18 23:24:17 +0100

    refactored code

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ let highScore = 0
 let secretNumber = Math.floor(Math.random() * 20) + 1
 let score = 20
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message
+}
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value)
 
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔️ No number!'
+    displayMessage('⛔️ No number!')
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Correct Number!'
+    displayMessage('🎉 Correct Number!')
     document.querySelector('.number').textContent = secretNumber
 
     document.querySelector('body').style.backgroundColor = '#60b347'
@@ -20,22 +24,13 @@ document.querySelector('.check').addEventListener('click', function () {
       highScore = score
       document.querySelector('.highscore').textContent = highScore
     }
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too high!'
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!')
       score--
       document.querySelector('.score').textContent = score
     } else {
-      document.querySelector('.message').textContent = '💥 you lost the game!'
-      document.querySelector('.score').textContent = 0
-    }
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Too low!'
-      score--
-      document.querySelector('.score').textContent = score
-    } else {
-      document.querySelector('.message').textContent = '💥 you lost the game!'
+      displayMessage('💥 you lost the game!')
       document.querySelector('.score').textContent = 0
     }
   }
