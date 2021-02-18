e2180714ad2804761bf3fa4db172f02d53a20604
xaviguasch <xaviguasch@gmail.com>
2021-02-18 23:09:24 +0100

    implemented reset and high score features

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 'use strict'
 
-const secretNumber = Math.floor(Math.random() * 20) + 1
+let highScore = 0
+let secretNumber = Math.floor(Math.random() * 20) + 1
 let score = 20
-
-document.querySelector('.number').textContent = secretNumber
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value)
@@ -12,6 +11,15 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '⛔️ No number!'
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!'
+    document.querySelector('.number').textContent = secretNumber
+
+    document.querySelector('body').style.backgroundColor = '#60b347'
+    document.querySelector('.number').style.width = '30rem'
+
+    if (score > highScore) {
+      highScore = score
+      document.querySelector('.highscore').textContent = highScore
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high!'
@@ -31,4 +39,19 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0
     }
   }
+})
+
+const againBtn = document.querySelector('.again')
+
+againBtn.addEventListener('click', function () {
+  score = 20
+  secretNumber = Math.floor(Math.random() * 20) + 1
+
+  document.querySelector('.message').textContent = 'Start guessing...'
+  document.querySelector('.score').textContent = score
+  document.querySelector('.number').textContent = '?'
+  document.querySelector('.guess').value = ''
+
+  document.querySelector('body').style.backgroundColor = '#222'
+  document.querySelector('.number').style.width = '15rem'
 })
